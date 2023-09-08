a6428f86508e94eb8c85a7b88387b24ca42daeb6
Hoàng Lâm <dev.nhlam@gmail.com>
2023-09-08 19:48:14 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,42 +19,44 @@ export default function RootLayout(props: RootLayoutProps) {
       <body>
         <ProviderComponent>{children}</ProviderComponent>
 
-        <section className="flex flex-row p-2 bg-slate-200 justify-center">
-          <Image
-            src="/logo/next-js.png"
-            className="hover:scale-110 transition-transform cursor-pointer"
-            width={40}
-            height={40}
-            style={{
-              objectFit: "cover",
-            }}
-            alt="react-icon"
-          />
-          <Image
-            src="/logo/nestjs-icon.png"
-            className="hover:scale-110 transition-transform cursor-pointer"
-            width={40}
-            height={40}
-            style={{
-              objectFit: "cover",
-            }}
-            alt="react-icon"
-          />
-          <Image
-            src="/logo/postgress-icon.png"
-            className="hover:scale-110 transition-transform cursor-pointer"
-            width={40}
-            height={40}
-            style={{
-              objectFit: "cover",
-            }}
-            alt="react-icon"
-          />
-        </section>
+        <section className="flex flex-col p-4  gap-2">
+          <div className="flex flex-row  gap-4 justify-center">
+            <Image
+              src="/logo/next-js.png"
+              className="hover:scale-110 transition-transform cursor-pointer"
+              width={40}
+              height={40}
+              style={{
+                objectFit: "cover",
+              }}
+              alt="react-icon"
+            />
+            <Image
+              src="/logo/nestjs-icon.png"
+              className="hover:scale-110 transition-transform cursor-pointer"
+              width={40}
+              height={40}
+              style={{
+                objectFit: "cover",
+              }}
+              alt="react-icon"
+            />
+            <Image
+              src="/logo/postgress-icon.png"
+              className="hover:scale-110 transition-transform cursor-pointer"
+              width={40}
+              height={40}
+              style={{
+                objectFit: "cover",
+              }}
+              alt="react-icon"
+            />
+          </div>
 
-        <p className="text-center text-sm font-semibold p-4">
-          Copyright ©2023 by Nguyen Hoang Lam
-        </p>
+          <p className="text-center text-sm font-semibold ">
+            Copyright ©2023 by Nguyen Hoang Lam
+          </p>
+        </section>
 
         <script
           defer
