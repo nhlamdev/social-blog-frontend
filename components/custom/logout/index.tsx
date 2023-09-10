e051fdf9f14b465532ae67ba14f924d6a8b61128
Hoàng Lâm <dev.nhlam@gmail.com>
2023-09-10 16:01:33 +0700

    update logout

--- components/custom/logout/index.tsx
+++ components/custom/logout/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-// import { callerInstance } from "@/api";
+import { apiCaller } from "@/api";
+import { useRouter } from "next/navigation";
 
 export const LogoutBtn = () => {
+  const router = useRouter();
   return (
     <div
       className="text-stone-600 bg-slate-900 dark:bg-cyan-600 px-4 py-2"
@@ -11,7 +13,11 @@ export const LogoutBtn = () => {
         cursor: "pointer",
         boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
       }}
-      // onClick={() => callerInstance.auth.logout()}
+      onClick={() => {
+        apiCaller.authApi.logout().then((res) => {
+          router.replace("/");
+        });
+      }}
     >
       <span className="text-sm text-slate-200 font-semibold">Đăng xuất</span>
     </div>
