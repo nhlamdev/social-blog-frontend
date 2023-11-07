07767f6dd9d10d7ae9e0bc6ed6bdb4ec41eb9fe1
Hoàng Lâm <dev.nhlam@gmail.com>
2023-11-08 06:42:47 +0700

    fix error rich editor

--- components/custom/text-editor/index.tsx
+++ components/custom/text-editor/index.tsx
@@ -1,76 +1,20 @@
 "use client";
+import { useMemo } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 
-const ImageResize = require("quill-image-resize-module-react");
-import { Quill } from "react-quill";
+// const ImageResize = require("quill-image-resize-module-react");
 
-const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
-  loading: () => <p>Loading...</p>,
-  ssr: false,
-});
+// const { Quill } = require("react-quill");
 
-Quill.register("modules/imageResize", ImageResize);
+// dynamic(() => require("react-quill").then((mod) => mod.Quill), {
+//   ssr: false,
+// }),
 
-const modules = {
-  toolbar: [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }, { font: [] }],
-    [{ size: [] }],
-    [
-      "bold",
-      "italic",
-      "underline",
-      "strike",
-      "blockquote",
-      "code-block",
-      { color: [] },
-      { background: [] },
-      { align: [] },
-    ],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image", "video"],
-    ["clean"],
-  ],
-  // imageResize: {
-  //   parchment: Quill.import("parchment"),
-  //   modules: ["Resize", "DisplaySize", "Toolbar"],
-  //   displayStyles: {
-  //     backgroundColor: "black",
-  //     border: "none",
-  //     color: "#fff",
-  //     // other camelCase styles for size display
-  //   },
-  // },
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
-};
-const formats = [
-  "header",
-  "width",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "align",
-  "image",
-  "video",
-  "color",
-  "background",
-];
+// const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
+//   loading: () => <p>Loading...</p>,
+//   ssr: false,
+// });
 
 interface MyTextEditorProps {
   width?: string;
@@ -83,8 +27,85 @@ interface MyTextEditorProps {
 export default function TextEditor(props: MyTextEditorProps): JSX.Element {
   const { backgroundColor, height, width, value, change } = props;
 
+  const { Quill } = require("react-quill");
+
+  const ImageResize = dynamic(
+    () => require("quill-image-resize-module-react"),
+    {
+      ssr: false,
+    }
+  );
+
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+
+  Quill.register("modules/imageResize", ImageResize);
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }, { font: [] }],
+      [{ size: [] }],
+      [
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "blockquote",
+        "code-block",
+        { color: [] },
+        { background: [] },
+        { align: [] },
+      ],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    imageResize: {
+      parchment: Quill.import("parchment"),
+      modules: ["Resize", "DisplaySize", "Toolbar"],
+      displayStyles: {
+        backgroundColor: "black",
+        border: "none",
+        color: "#fff",
+        // other camelCase styles for size display
+      },
+    },
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const formats = [
+    "header",
+    "width",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "align",
+    "image",
+    "video",
+    "color",
+    "background",
+  ];
+
   return (
-    <QuillNoSSRWrapper
+    <ReactQuill
       modules={modules}
       formats={formats}
       theme="snow"
