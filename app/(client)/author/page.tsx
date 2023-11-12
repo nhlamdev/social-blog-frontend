e7d8b32d80e90abba5c4860020b9cefc5e23b59f
Hoàng Lâm <dev.nhlam@gmail.com>
2023-11-12 20:42:17 +0700

    make pagination fo author page

--- app/(client)/author/page.tsx
+++ app/(client)/author/page.tsx
@@ -1,16 +1,44 @@
+import { PaginationDirectComponent } from "@/components/custom";
 import { ListViewAuthors } from "@/components/list-view/list-author";
+import { PageProps } from "@/interface";
+import { generateURLWithQueryParams, getCountPage } from "@/utils/global-func";
 import axios from "axios";
 import Image from "next/image";
 const backend = process.env.SERVICE_PORT;
 
-export default async function AuthorContentPage() {
+export default async function AuthorContentPage(props: PageProps) {
+  const { searchParams } = props;
+
+  const { page, series } = searchParams;
+
+  const current =
+    page && !Number.isNaN(Number(page)) && Number.isInteger(Number(page))
+      ? Number(page) - 1
+      : 0;
+
   const {
     data: [members, count],
-  } = await axios.get(`http://localhost:${backend}/content/all-author`);
+  } = await axios.get(`http://localhost:${backend}/content/all-author`, {
+    params: { ...searchParams, skip: (current * 5).toString(), take: "5" },
+  });
 
   return (
-    <div className="min-h-screen flex flex-row w-full p-4 gap-4 justify-center">
+    <div className="flex-1 flex flex-col w-full p-4 gap-4  items-center">
       <ListViewAuthors members={members} />
+
+      {members && count !== 0 ? (
+        <PaginationDirectComponent
+          current={current + 1}
+          total={getCountPage(count, 5)}
+          urlDirect={(p: number) => {
+            const clone = { ...searchParams, page: p.toString() };
+
+            return generateURLWithQueryParams("/author", clone);
+          }}
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
--- components/list-view/list-author/index.tsx
+++ components/list-view/list-author/index.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { BiBookContent } from "react-icons/bi";
 import { BsFillBookmarksFill } from "react-icons/bs";
 import { ListViewAuthorsItem } from "./item";
+import { PaginationDirectComponent } from "@/components/custom";
+import { generateURLWithQueryParams, getCountPage } from "@/utils/global-func";
 
 interface IListViewAuthors {
   members: any[];
@@ -11,7 +13,7 @@ interface IListViewAuthors {
 export const ListViewAuthors = (props: IListViewAuthors) => {
   const { members } = props;
   return (
-    <div className="flex flex-col w-3/5 gap-2">
+    <div className="flex flex-col w-3/5 gap-2 flex-1">
       {members.map((member: any) => {
         return <ListViewAuthorsItem key={member._id} member={member} />;
       })}
